4bf8ccebd6d71fb76bc53e8b0e50eda5980770a6
prernasyan <prernasyan22@gmail.com>
2023-04-13 13:26:19 +0530

    add new ticket form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
-import { Button } from 'react-bootstrap';
-import { Entry } from './page/entry/Entry.page';
+//import { Button } from 'react-bootstrap';
+//import { Entry } from './page/entry/Entry.page';
 import { DefaultLayout } from './layout/DefaultLayout';
-import { Dashboard } from './page/dashboard/Dashboard.page';
+//import { Dashboard } from './page/dashboard/Dashboard.page';
+import { AddTicket } from './page/new-ticket/AddTicket.page';
 
 function App() {
   return (
     <div className="App">
       {/*<Entry /> */}
       <DefaultLayout>
-        <Dashboard />
+        {/* <Dashboard />*/}
+        <AddTicket /> 
       </DefaultLayout>
     </div>
   );
