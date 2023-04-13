2380fcb3b978ed4882e0c620b3732d4dae99abea
prernasyan <prernasyan22@gmail.com>
2023-04-13 12:15:08 +0530

    dashboard created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import { Button } from 'react-bootstrap';
 import { Entry } from './page/entry/Entry.page';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './page/dashboard/Dashboard.page';
 
 function App() {
   return (
     <div className="App">
       {/*<Entry /> */}
-      <DefaultLayout />
+      <DefaultLayout>
+        <Dashboard />
+      </DefaultLayout>
     </div>
   );
 }
