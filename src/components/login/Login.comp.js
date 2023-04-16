9472ae21d3a9910ad4dec357f4aaf88778205ed8
prernasyan <prernasyan22@gmail.com>
2023-04-16 13:18:51 +0530

    login component updated

--- src/components/login/Login.comp.js
+++ src/components/login/Login.comp.js
@@ -1,63 +1,129 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import {Container,Row, Col, Form, Button} from 'react-bootstrap'
- 
-export const LoginForm = ({
-    handleOnChange,
-    handleOnSubmit, 
-    formSwitcher,
-    email,
-    pass
-    }) => {
-    return (
-        <Container>
-            <Row>
-                <Col>
-                <h1 className="text-info text center"> Client Login </h1>
-                <hr />
-                <Form autoComplete="off" onSubmit={handleOnSubmit}>
-                    <Form.Group>
-                        <Form.Label> Email Address </Form.Label>
-                        <Form.Control
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import {
+	Container,
+	Row,
+	Col,
+	Form,
+	Button,
+	Spinner,
+	Alert,
+} from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useLocation } from "react-router-dom";
+
+import { loginPending, loginSuccess, loginFail } from "./loginSlice";
+import { userLogin } from "../../api/userApi";
+import { getUserProfile } from "../../pages/dashboard/userAction";
+
+export const LoginForm = ({ formSwitcher }) => {
+	const dispatch = useDispatch();
+	const history = useHistory();
+	let location = useLocation();
+
+	const { isLoading, isAuth, error } = useSelector(state => state.login);
+	let { from } = location.state || { from: { pathname: "/" } };
+
+	useEffect(() => {
+		sessionStorage.getItem("accessJWT") && history.replace(from);
+	}, [history, isAuth]);
+
+	const [email, setEmail] = useState("e2@e.com");
+	const [password, setPassword] = useState("password#1F");
+
+	const handleOnChange = e => {
+		const { name, value } = e.target;
+
+		switch (name) {
+			case "email":
+				setEmail(value);
+				break;
+
+			case "password":
+				setPassword(value);
+				break;
+
+			default:
+				break;
+		}
+	};
+
+	const handleOnSubmit = async e => {
+		e.preventDefault();
+
+		if (!email || !password) {
+			return alert("Fill up all the form!");
+		}
+
+		dispatch(loginPending());
+
+		try {
+			const isAuth = await userLogin({ email, password });
+
+			if (isAuth.status === "error") {
+				return dispatch(loginFail(isAuth.message));
+			}
+
+			dispatch(loginSuccess());
+			dispatch(getUserProfile());
+			history.push("/dashboard");
+		} catch (error) {
+			dispatch(loginFail(error.message));
+		}
+	};
+
+	return (
+		<Container>
+			<Row>
+				<Col>
+					<h1 className="text-info text-center">Client Login</h1>
+					<hr />
+					{error && <Alert variant="danger">{error}</Alert>}
+					<Form autoComplete="off" onSubmit={handleOnSubmit}>
+						<Form.Group>
+							<Form.Label>Email Address</Form.Label>
+							<Form.Control
 								type="email"
 								name="email"
-                                value={email}
-                                onChange={handleOnChange}
+								value={email}
+								onChange={handleOnChange}
 								placeholder="Enter Email"
-                                required
+								required
 							/>
-                    </Form.Group>
-                    <Form.Group> 
-							<Form.Label>Password</Form.Label> 
+						</Form.Group>
+						<Form.Group>
+							<Form.Label>Password</Form.Label>
 							<Form.Control
 								type="password"
 								name="password"
-                                value={pass}
-                                onChange={handleOnChange}
+								onChange={handleOnChange}
+								value={password}
 								placeholder="password"
-                                required
+								required
 							/>
 						</Form.Group>
 
 						<Button type="submit">Login</Button>
-						
-                </Form>
-                <hr />
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                <a href="#!"onClick={() => formSwitcher("rest")}> Forget Password </a>
-                </Col>
-            </Row>
+						{isLoading && <Spinner variant="primary" animation="border" />}
+					</Form>
+					<hr />
+				</Col>
+			</Row>
 
-        </Container>
-    )
-}
-LoginForm.propTypes={
-    handleOnChange: PropTypes.func.isRequired,
-    handleOnSubmit: PropTypes.func.isRequired,
-    formSwitcher: PropTypes.func.isRequired,
-    email: PropTypes.string.isRequired,
-    pass: PropTypes.string.isRequired,
+			<Row>
+				<Col>
+					<a href="/password-reset">Forget Password?</a>
+				</Col>
+			</Row>
+			<Row className="py-4">
+				<Col>
+					Are you new here? <a href="/registration">Register Now</a>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+
+LoginForm.propTypes = {
+	formSwitcher: PropTypes.func.isRequired,
 };
